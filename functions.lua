5a0268e9fd672f2f5bd4b1e0b5b24515d78c5f23
Adam Debniak <mlodszyboss123@gmail.com>
2021-11-19 20:36:06 +0100

    basic collision
    added a function for handling some basic collision

--- functions.lua
+++ functions.lua
@@ -1,4 +1,28 @@
-function detectColision(ballCenter, ballVelocity, world)
+local functions = {}
+
+function functions.detectColision(ballCenter, ballVelocity, dt, world)
+    
+    if 0 <= ballCenter[1] - ballRadius and ballCenter[1] + ballRadius <= 800 then 
+        ballCenter[1] = ballCenter[1] + ballVelocity[1]*dt
+    elseif ballCenter[1] + ballRadius > 800 then
+        ballCenter[1] = 800 - ballRadius
+        ballVelocity[1] = -ballVelocity[1]
+    else
+        ballCenter[1] = 0 + ballRadius
+        ballVelocity[1] = -ballVelocity[1]
+    end
+
+    if 0 <= ballCenter[2] - ballRadius and ballCenter[2] + ballRadius <= 600 then
+        ballCenter[2] = ballCenter[2] + ballVelocity[2]*dt
+    elseif ballCenter[2] + ballRadius > 600 then
+        ballCenter[2] = 600 - ballRadius
+        ballVelocity[2] = -ballVelocity[2]
+    else
+        ballCenter[2] = 0 + ballRadius
+        ballVelocity[2] = -ballVelocity[2]
+    end
     
     return ballCenter, ballVelocity
 end
+
+return functions
