92d7bcc6b0deadfa2ccb63076310f20cdc69310b
Adam Debniak <mlodszyboss123@gmail.com>
2021-11-26 19:32:28 +0100

    homework

--- functions.lua
+++ functions.lua
@@ -25,33 +25,38 @@ functions.ballMovement = function(ball, dt, world)
 end
 
 function functions.paddleMovement(rocketL, rocketR, rocketSpeed, dt, world)
-    if love.keyboard.isDown("w") then 
+    
+    if love.keyboard.isDown("w") and rocketL[6] > 0 then 
         rocketL[2] = rocketL[2] - rocketSpeed * dt
         rocketL[4] = rocketL[4] - rocketSpeed * dt
         rocketL[6] = rocketL[6] - rocketSpeed * dt
         rocketL[8] = rocketL[8] - rocketSpeed * dt
     end
 
-    if love.keyboard.isDown("s") then 
+    if love.keyboard.isDown("s") and rocketL[2] < world.height then 
         rocketL[2] = rocketL[2] + rocketSpeed * dt
         rocketL[4] = rocketL[4] + rocketSpeed * dt
         rocketL[6] = rocketL[6] + rocketSpeed * dt
         rocketL[8] = rocketL[8] + rocketSpeed * dt
     end
 
-    if love.keyboard.isDown("o") then 
+    if love.keyboard.isDown("o") and rocketR[6] > 0 then 
         rocketR[2] = rocketR[2] - rocketSpeed * dt
         rocketR[4] = rocketR[4] - rocketSpeed * dt
         rocketR[6] = rocketR[6] - rocketSpeed * dt
         rocketR[8] = rocketR[8] - rocketSpeed * dt
     end
 
-    if love.keyboard.isDown("l") then 
+    if love.keyboard.isDown("l") and rocketR[2] < world.height then 
         rocketR[2] = rocketR[2] + rocketSpeed * dt
         rocketR[4] = rocketR[4] + rocketSpeed * dt
         rocketR[6] = rocketR[6] + rocketSpeed * dt
         rocketR[8] = rocketR[8] + rocketSpeed * dt
     end
+
+    -- zrobić żeby piłka odbijała się od paletek, sprawdzać czy jest szansa na zderzenie przez sprawdzenie koordynatów.
+
+    
 end
 
 return functions
