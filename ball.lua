16d2053f5e6b0a4c9178ffd205846a6f275533d3
Adam Debniak <mlodszyboss123@gmail.com>
2021-12-17 18:25:03 +0100

    One more step into the "alternative-way"
    alternative-way - our great framework

--- ball.lua
+++ ball.lua
@@ -1,14 +1,17 @@
 local ball = {
-    center = {400, 300}
-    radius = 50
-    velocity = {100, 110}
+    center = {400, 300},
+    radius = 50,
+    velocity = {100, 110},
     -- to moze byc troche tricky
     -- zaprogramuj to wedle wlasnego pomyslu
     -- ale nie uzywaj w programiwe
-    bounce = function (rockets, world)
-        if center[1] - radius < edges.x1 and if center[2] < edges.x4 and center[2] > edges.x3 then
-            velocity[1] = -velocity[1]
-        end
+    bounce = function (world, rockets)
+        love.graphics.print(rockets.left.edges.L, 20, 20)
+
+
+        -- if center[1] - radius < edges.x1 and if center[2] < edges.x4 and center[2] > edges.x3 then
+        --     velocity[1] = -velocity[1]
+        -- end
 
         -- to jest jakis przyklad
         -- to neikoniecznie jset najlepszy pomysl
--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
-local f = require("functions")
+local functions = require("functions")
+local rockets = require("rockets")
+local ball = require("ball")
 
-local ball = {}
-ball.radius = 50
-ball.color = {0.5, 0.5, 0.5}
-ball.velocity = {100, 110}
+-- local ball = {}
+-- ball.radius = 50
+-- ball.color = {0.5, 0.5, 0.5}
+-- ball.velocity = {100, 110}
 
 local world = {}
 
@@ -20,9 +22,9 @@ local rocketR = {
     775,500
 }
 
-local rockets = {}
-rockets.left = {}
-rocket.edges = {}
+-- local rockets = {}
+-- rockets.left = {}
+-- rocket.edges = {}
 
 
 rockets.right = {}
@@ -36,31 +38,35 @@ function love.load()
 end
 
 function love.update(dt)
-    f.paddleMovement(rocketL, rocketR, rocketSpeed, dt, world)
-    -- rockets.dealWithMovementDERP(dt, world)
-    f.ballMovement(ball, dt, world)
-    f.ballBounce(ball, rocketL, rocketR, world, dt)
+
+--     functions.paddleMovement(rocketL, rocketR, rocketSpeed, dt, world)
+--     -- rockets.dealWithMovementDERP(dt, world)
+--     functions.ballMovement(ball, dt, world)
+--     functions.ballBounce(ball, rocketL, rocketR, world, dt)
 end
 
 function love.draw()
-    love.graphics.setColor(
-        ball.color[1],
-        ball.color[2],
-        ball.color[3])
-    love.graphics.circle(
-        "fill",
-        ball.center[1],
-        ball.center[2],
-        ball.radius
-    )
-    love.graphics.setColor(1,1,1)
-    love.graphics.polygon(
-        "fill",
-        rocketL
-    )
-    love.graphics.polygon(
-        "fill",
-        rocketR
-    )
+    ball.bounce(world, rockets) -- to nie ma być tu.
+    -- rockets.left.draw()
+
+    -- love.graphics.setColor(
+    --     ball.color[1],
+    --     ball.color[2],
+    --     ball.color[3])
+    -- love.graphics.circle(
+    --     "fill",
+    --     ball.center[1],
+    --     ball.center[2],
+    --     ball.radius
+    -- )
+    -- love.graphics.setColor(1,1,1)
+    -- love.graphics.polygon(
+    --     "fill",
+    --     rocketL
+    -- )
+    -- love.graphics.polygon(
+    --     "fill",
+    --     rocketR
+    -- )
 
 end
